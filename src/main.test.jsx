da0856df6a12c0fa6e37f1d2cf6b47d862601f6d
Angely Liced Herrera Roldan <183932452+angely01-1@users.noreply.github.com>
2025-11-21 20:49:20 -0500

    corrección v6

--- src/main.test.jsx
+++ src/main.test.jsx
@@ -10,7 +10,7 @@ describe('main.jsx', () => {
     const renderMock = vi.fn();
     vi.spyOn(document, 'getElementById').mockReturnValue(rootDiv);
     vi.spyOn(require('react-dom/client'), 'createRoot').mockReturnValue({ render: renderMock });
-    require('./main');
+    require('./main.jsx');
     expect(renderMock).toHaveBeenCalledWith(<App />);
     document.body.removeChild(rootDiv);
   });
