f8b1e6d9c1bb8290fea87f533fd273a2fe372edd
Angely Liced Herrera Roldan <183932452+angely01-1@users.noreply.github.com>
2025-11-21 20:46:54 -0500

    corrección v5

--- src/main.test.jsx
+++ src/main.test.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { vi } from 'vitest';
 import App from './App';
 
 describe('main.jsx', () => {
   it('should render App without crashing', () => {
-    // Mock document.getElementById and createRoot
     const rootDiv = document.createElement('div');
     rootDiv.setAttribute('id', 'root');
     document.body.appendChild(rootDiv);
-    const renderMock = jest.fn();
-    jest.spyOn(document, 'getElementById').mockReturnValue(rootDiv);
-    jest.spyOn(require('react-dom/client'), 'createRoot').mockReturnValue({ render: renderMock });
+    const renderMock = vi.fn();
+    vi.spyOn(document, 'getElementById').mockReturnValue(rootDiv);
+    vi.spyOn(require('react-dom/client'), 'createRoot').mockReturnValue({ render: renderMock });
     require('./main');
     expect(renderMock).toHaveBeenCalledWith(<App />);
     document.body.removeChild(rootDiv);
